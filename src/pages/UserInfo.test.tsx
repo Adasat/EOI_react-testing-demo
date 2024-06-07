49b866a8698c54f2359d6d398b93666b3e863723
NadiaMonzon <nadiamonque99@gmail.com>
2024-06-07 20:00:17 +0100

    test(user info): continue integration tests on filters and delete unused code

--- src/pages/UserInfo.test.tsx
+++ src/pages/UserInfo.test.tsx
@@ -1,49 +1,47 @@
-import React from 'react';
-import { render, screen, waitFor, fireEvent, getByText, getByTestId } from '@testing-library/react';
-import { UserInfo } from './UserInfo';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { getUsers } from '../services/userService';
-import { FilterInput } from '../components/FilterInput/FilterInput';
-import { wait } from '@testing-library/user-event/dist/utils';
+import { UserInfo } from './UserInfo';
 
 let numOfPeople = 5
-let users: any; 
+let users: any;
 jest.mock('../services/userService', () => ({
   getUsers: jest.fn(),
 }));
-describe.skip('Unit test', ()=>{
+
+describe.skip('Unit test', () => {
   beforeEach(() => {
     (getUsers as jest.Mock).mockImplementation(() => Promise.resolve({
       json: () => Promise.resolve([
-        {name: 'John Doe'},
-        {name: 'Jane Doe'},
-        {name: 'John Smith'},
-        {name: 'Jane Smith'},
-        {name: 'faS Doe'},
+        { name: 'John Doe' },
+        { name: 'Jane Doe' },
+        { name: 'John Smith' },
+        { name: 'Jane Smith' },
+        { name: 'faS Doe' },
       ])
     }));
   });
-      
+
   test('Userinfo should recieve an any object as promise', async () => {
     (getUsers as jest.Mock).mockImplementation(() => Promise.resolve({
-      json: () =>[]
+      json: () => []
     }));
     await render(<UserInfo />);
     await waitFor(async () => {
-      expect(  screen.queryByRole('listitem')).not.toBeInTheDocument()
+      expect(screen.queryByRole('listitem')).not.toBeInTheDocument()
     })
   })
   test('deleteUserButton should not exist on the list if its empty', async () => {
     (getUsers as jest.Mock).mockImplementation(() => Promise.resolve({
-      json: () =>[]
+      json: () => []
     }));
     render(<UserInfo />)
     await waitFor(async () => {
-      expect(  screen.queryByText('Delete') ).not.toBeInTheDocument()
+      expect(screen.queryByText('Delete')).not.toBeInTheDocument()
     })
   })
 
   test('deleteUserButton should remove users from list', async () => {
-    await render(<UserInfo/>)
+    await render(<UserInfo />)
     await waitFor(() => {
       users = screen.getAllByRole('listitem')
     })
@@ -59,97 +57,99 @@ describe.skip('Unit test', ()=>{
     expect(users).toHaveLength(4)
   })
 })
-describe ('Functional test', () => {
-    
+describe('Functional test', () => {
+
   beforeEach(() => {
     (getUsers as jest.Mock).mockImplementation(() => Promise.resolve({
       json: () => Promise.resolve([
-        {name: 'John Doe'},
-        {name: 'Jane Doe'},
-        {name: 'John Smith'},
-        {name: 'Jane Smith'},
-        {name: 'faS Doe'},
+        { name: 'John Doe' },
+        { name: 'Jane Doe' },
+        { name: 'John Smith' },
+        { name: 'Jane Smith' },
+        { name: 'faS Doe' },
       ])
     }));
   });
   afterEach(() => {
     numOfPeople = 5
   })
-  
+
   test('userContainer should be have 5 children by default', async () => {
     await render(<UserInfo />);
-      await waitFor(() => {
-        users = screen.getAllByRole('listitem')
-      })
-      expect(users).toHaveLength(numOfPeople)
-    
-  })
-
-  test('addUserButton should increase numOfPeopleValue' , async () => {
-    await render(<UserInfo/>)
-      let button: any
-      let newUsers: any
-    await waitFor(() => {
-      users = screen.getAllByRole('listitem')
-    })
-      button = screen.getByText('Add User')
-
-    await waitFor (() =>{
-
-      // eslint-disable-next-line testing-library/no-wait-for-side-effects
-      fireEvent.click(button)
-      numOfPeople++
-    })
-      newUsers = screen.getAllByRole('listitem')
-      expect(newUsers).toHaveLength(numOfPeople)
-  })
-
-  const setup = () => {
-
-    const utils = render(<FilterInput onInputChange={function (e: string): void {
-  
-      throw new Error('Function not implemented.');
-  
-    } }  />)
-  
-    const input = screen.getByLabelText('Search user...')
-  
-    const list = screen.getByRole('list')
-  
-    return {
-  
-      input,
-  
-      ...utils,
-  
-    }
-  
-  }
-  
-  test.only('filter input should filter data from ul list', async () => {
-    await render(<UserInfo/>)
-    await waitFor(() => {
-      users = screen.getAllByRole('listitem')
-    })
-    let input =  screen.getByPlaceholderText('Search user...')   
-    fireEvent.change(input, {target: {value: 'Jane Doe'}})
-    await waitFor(() => {
-      users = screen.getAllByRole('listitem')
-    })
-    expect(users).toHaveLength(1)
-
-
-  })
-  test.only('5 items should exist if filter input is empty', async () => {
-    await render(<UserInfo/>)
-    let input =  screen.getByPlaceholderText('Search user...')   
-    fireEvent.change(input, {target: {value: ''}})
     await waitFor(() => {
       users = screen.getAllByRole('listitem')
     })
     expect(users).toHaveLength(numOfPeople)
 
-
   })
+
+  test('addUserButton should increase numOfPeopleValue', async () => {
+    await render(<UserInfo />)
+    let button: any
+    let newUsers: any
+    await waitFor(() => {
+      users = screen.getAllByRole('listitem')
+    })
+    button = screen.getByText('Add User')
+
+    await waitFor(() => {
+
+      // eslint-disable-next-line testing-library/no-wait-for-side-effects
+      fireEvent.click(button)
+      numOfPeople++
+    })
+    newUsers = screen.getAllByRole('listitem')
+    expect(newUsers).toHaveLength(numOfPeople)
+  })
+
+  describe('Filters tests', () => {
+
+    test('FilterInput element should exist on the document', async () => {
+      await render(<UserInfo />)
+      let input = screen.getByPlaceholderText('Search user...')
+      expect(input).toBeInTheDocument()
+    })
+
+    test('filter input should filter data from ul list', async () => {
+      await render(<UserInfo />)
+      await waitFor(() => {
+        users = screen.getAllByRole('listitem')
+      })
+      let input = screen.getByPlaceholderText('Search user...')
+      fireEvent.change(input, { target: { value: 'Jane Doe' } })
+      await waitFor(() => {
+        users = screen.getAllByRole('listitem')
+      })
+      expect(users).toHaveLength(1)
+
+
+    })
+    test('5 items should exist if filter input is empty', async () => {
+      await render(<UserInfo />)
+      let input = screen.getByPlaceholderText('Search user...')
+      await waitFor(() => {
+        users = screen.getAllByRole('listitem')
+      })
+      fireEvent.change(input, { target: { value: '' } })
+      await waitFor(() => {
+        users = screen.getAllByRole('listitem')
+      })
+      expect(users).toHaveLength(numOfPeople)
+    })
+
+    //TODO: fix this test :)
+    test('user list should be empty if filter cant find any coincidences', async () => {
+      await render(<UserInfo />)
+      let input = screen.getByPlaceholderText('Search user...')
+      await waitFor(() => {
+        users = screen.getAllByRole('listitem')
+      })
+      fireEvent.change(input, { target: { value: 'Non-existing user' } })
+      await waitFor(() => {
+        users = screen.queryAllByRole('listitem')
+      })
+      expect(users).not.toBeInTheDocument()
+    })
+  })
+
 })
- 
--- src/pages/UserInfo.tsx
+++ src/pages/UserInfo.tsx
@@ -1,67 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import { getUsers } from '../services/userService';
+import { useEffect, useState } from 'react';
 import { CustomForm } from '../components/CustomButton/CustomForm';
+import { getUsers } from '../services/userService';
 
-import './UserInfo.css'
 import { FilterInput } from '../components/FilterInput/FilterInput';
+import './UserInfo.css';
 
 
 export const UserInfo = () => {
-    const [list, setList] = useState<any[]>([])
-    const [count, setCount] = useState<Number>(5)
-    const [filteredList, setFilteredList] = useState<any[]>([])
+  const [list, setList] = useState<any[]>([])
+  const [count, setCount] = useState<Number>(5)
+  const [filteredList, setFilteredList] = useState<any[]>([])
 
-    useEffect(() => {
-        let getUserFunction = async () => {
-            const response = await getUsers(count)
-            const data = await response.json();
-            setList(data);
-            setFilteredList(data)
+  useEffect(() => {
+    let getUserFunction = async () => {
+      const response = await getUsers(count)
+      const data = await response.json();
+      setList(data);
+      setFilteredList(data)
 
-        }
-        getUserFunction()
-    }, [])
-
-    const handleFilteredList = (user: string) => {
-      console.log(typeof user)
-        if(user !== ''){
-          setFilteredList(list?.filter((e) => e?.name?.toLowerCase().includes(user.toLowerCase())))
-        } else{
-          setFilteredList(list)
-        }
-    } 
-
-    const handleChange = (user: {name: string}): void => {
-        setList((preList)=> { return [...preList, user];});
-        setFilteredList((preList)=> { return [...preList, user];})
     }
-    
-    const removeUser = (userName: string) => {
-      console.log('click on delete button')
-      setList(list.filter((user) => user.name !== userName))
-      setFilteredList(filteredList.filter((user) => user.name !== userName))
+    getUserFunction()
+  }, [])
+
+  const handleFilteredList = (user: string) => {
+    console.log(typeof user)
+    if (user !== '') {
+      setFilteredList(list?.filter((e) => e?.name?.toLowerCase().includes(user.toLowerCase())))
+    } else {
+      setFilteredList(list)
     }
+  }
+
+  const handleChange = (user: { name: string }): void => {
+    setList((preList) => { return [...preList, user]; });
+    setFilteredList((preList) => { return [...preList, user]; })
+  }
+
+  const removeUser = (userName: string) => {
+    console.log('click on delete button')
+    setList(list.filter((user) => user.name !== userName))
+    setFilteredList(filteredList.filter((user) => user.name !== userName))
+  }
 
 
-    useEffect(() => {
-    }, [list,filteredList]);
+  useEffect(() => {
+  }, [list, filteredList]);
 
   return (
-    <div id='userContainer'  data-testid='userContainer'>
-        <FilterInput onInputChange={handleFilteredList}/>
-     
+    <div id='userContainer' data-testid='userContainer'>
+      <FilterInput onInputChange={handleFilteredList} />
+
       {
-      
-      filteredList.map((user, i) => (
-        <div key={i} className='listContainer'>
-          <li>{user.name}</li>
-          <button
-            className='deleteUser'
-            aria-label='deleteBtn'
-            onClick={() => removeUser(user.name)}
-          >Delete</button>
-        </div>
-      ))}
+
+        filteredList.map((user, i) => (
+          <div key={i} className='listContainer'>
+            <li>{user.name}</li>
+            <button
+              className='deleteUser'
+              aria-label='deleteBtn'
+              onClick={() => removeUser(user.name)}
+            >Delete</button>
+          </div>
+        ))}
 
       <CustomForm handleChange={handleChange} />
     </div>
